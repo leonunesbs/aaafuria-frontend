708cd268e0f1914f0bf894b1a7e573d0a5f7ee36
Leonardo Nunes <leonunesbs@gmail.com>
2022-08-12 22:12:00 -0300

    fix(url): fiz redirect

--- src/pages/intermed.tsx
+++ src/pages/intermed.tsx
@@ -478,7 +478,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   if (!token) {
     return {
       redirect: {
-        destination: `/entrar?after=${ctx.resolvedUrl}`,
+        destination: `/login?after=${ctx.resolvedUrl}`,
         permanent: false,
       },
     };
