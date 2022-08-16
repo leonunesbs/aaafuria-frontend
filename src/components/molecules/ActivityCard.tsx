bb561a25888ada0227ff05ce3752e8019b59a1a0
Leonardo Nunes <leonunesbs@gmail.com>
2022-08-16 15:28:49 -0300

    fix(auth): show to coord

--- src/components/molecules/ActivityCard.tsx
+++ src/components/molecules/ActivityCard.tsx
@@ -24,6 +24,7 @@ interface ActivityCardProps {
 export function ActivityCard({ activity, refetch }: ActivityCardProps) {
   const { user } = useContext(AuthContext);
   const activityName = useMemo(() => activity.name, [activity.name]);
+  console.log(user);
   return (
     <Card w="full" py={6} px={[4, 6]}>
       <HStack mb={2} w="full" justify={'space-between'} pr={1}>
@@ -40,7 +41,7 @@ export function ActivityCard({ activity, refetch }: ActivityCardProps) {
             <Heading size="sm">{activity.name.toUpperCase()}</Heading>
           </CustomLink>
         </HStack>
-        {user?.isStaff && (
+        {(user?.member.isCoordinator || user?.isStaff) && (
           <Box>
             <AddScheduleDrawer refetch={refetch} activityId={activity.id} />
           </Box>
