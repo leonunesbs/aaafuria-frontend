099f245d32a352c5676d2efc95d9579c3b331b85
Leonardo Nunes <leonunesbs@gmail.com>
2022-08-31 13:25:09 -0300

    fix(fix): pwa

--- next.config.js
+++ next.config.js
@@ -1,40 +1,30 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 const removeImports = require('next-remove-imports')();
 
-const nextConfig = withPlugins(
-  [
-    [
-      withPWA,
-      {
-        dest: 'public',
-        register: true,
-        disable: process.env.NODE_ENV === 'development',
-      },
-    ],
-    [removeImports, {}],
-  ],
-  {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      runtime: 'experimental-edge',
-    },
-    images: {
-      domains: [
-        'aaafuria-reborn.s3.amazonaws.com',
-        'aaafuria-reborn.s3.sa-east-1.amazonaws.com',
-      ],
-    },
-    env: {
-      BACKEND_DOMAIN: 'https://backend.aaafuria.site',
-      DIRETORIA_DOMAIN: 'https://diretoria.aaafuria.site',
-      NEXT_PUBLIC_GA_ID: 'G-Q55G1EGX2K',
-      PUBLIC_AWS_URI:
-        'https://aaafuria-reborn.s3.sa-east-1.amazonaws.com/public',
-    },
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    runtime: 'experimental-edge',
   },
-);
+  images: {
+    domains: [
+      'aaafuria-reborn.s3.amazonaws.com',
+      'aaafuria-reborn.s3.sa-east-1.amazonaws.com',
+    ],
+  },
+  env: {
+    BACKEND_DOMAIN: 'https://backend.aaafuria.site',
+    DIRETORIA_DOMAIN: 'https://diretoria.aaafuria.site',
+    NEXT_PUBLIC_GA_ID: 'G-K5LPGWWJL1',
+    PUBLIC_AWS_URI: 'https://aaafuria-reborn.s3.sa-east-1.amazonaws.com/public',
+  },
+};
 
-module.exports = nextConfig;
+module.exports = withPlugins([[withPWA], [removeImports, {}]], nextConfig);
