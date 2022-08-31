651012ed8685c008b8fbaec42331439f16b14519
Leonardo Nunes <leonunesbs@gmail.com>
2022-08-30 21:00:03 -0300

    style(fix): colorMode

--- src/components/atoms/ColorModeToggle.tsx
+++ src/components/atoms/ColorModeToggle.tsx
@@ -1,19 +1,25 @@
-import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 import { useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { ReactNode, useEffect } from 'react';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 
 import { CustomIconButton } from './CustomIconButton';
-import { ReactNode } from 'react';
 
 interface ColorModeToggleProps {
   children?: ReactNode;
 }
 
 export function ColorModeToggle({}: ColorModeToggleProps) {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
   const icon = useColorModeValue(
     <BsFillMoonFill size="20px" />,
     <BsFillSunFill size="20px" />,
   );
+
+  useEffect(() => {
+    if (document) {
+      document.documentElement.setAttribute('data-color-mode', colorMode);
+    }
+  }, [colorMode]);
   return (
     <CustomIconButton
       aria-label="colorMode"
